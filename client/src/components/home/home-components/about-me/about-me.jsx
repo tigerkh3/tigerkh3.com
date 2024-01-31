115a357ca69acbc6f74c31f75e2c4028934e4c61
Tiger Hong <tigerhong@Tigers-MBP.lan>
2024-01-31 15:43:06 -0800

    css-dev-1 | about me css
    Home Page intro section css complete.

--- client/src/components/home/home-components/about-me/about-me.jsx
+++ client/src/components/home/home-components/about-me/about-me.jsx
@@ -12,7 +12,11 @@ function AboutMeHome () {
   return ([
     <Container key="about-me-home" className="about-me-home-parent">
       <Row key="about-me-home-row" className="about-me-home-description">
-        {sampleAM.homePage}
+        {sampleAM.homePage.split('\n').map((currentParagraph, index) => {
+          return (
+            <h4>{currentParagraph}</h4>
+          )
+        })}
       </Row>
     </Container>
   ])
