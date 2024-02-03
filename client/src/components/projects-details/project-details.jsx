7ccfeb2ee3bfbc853bb18e2316f36ae132105e36
Tiger Hong <tigerhong@Tigers-MBP.lan>
2024-02-02 16:12:50 -0800

    css-dev-1 | project details anchor
    Project Details now has an achor id tag that allows for users to jump directly to the sections they'd like from the table of contents.

--- client/src/components/projects-details/project-details.jsx
+++ client/src/components/projects-details/project-details.jsx
@@ -4,6 +4,7 @@ import "../../dist/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row } from "reactstrap";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
 // sample projects database object
 import sampleRM from "./sample/project-details-sample.js"
 
@@ -30,9 +31,26 @@ function ProjectDetails () {
     })
   })
 
+  const generateSlug = (string) => {
+    let str = string.replace(/^\s+|\s+$/g, "");
+    str = str.toLowerCase();
+    str = str
+      .replace(/[^a-z0-9 -]/g, "")
+      .replace(/\s+/g, "-")
+      .replace(/-+/g, "-");
+    return str;
+  }
+
   return (
     <Container key="project-details-main-parent" className="project-details-main-parent">
-      <ReactMarkdown key="project-details-main" className="project-details-main" children={markdown}></ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+           h1: ({ node, ...props }) => (
+             <h1 id={generateSlug(props.children)} {...props}></h1>
+           ),
+         }}
+         key="project-details-main" className="project-details-main" children={markdown}></ReactMarkdown>
     </Container>
   )
 }
