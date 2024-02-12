49566665daf2f6928505771a2e7570478aa181e4
Tiger Hong <tigerhong@Tigers-MBP.lan>
2024-02-12 15:59:48 -0800

    project-display-dev-3 | overlay CSS update
    Project display component css updated.
    
    Prior css was working fine, but I decided to change the cards from small to large to emphasize my HQ projects more.
    
    Custom viewport width css has been added for the overlay to match up with with responsive components that it's covering. Sadly I wasn't able to figure out a method that allowed an absolute positioned element to naturally follow it's prior parent (not sure if it's possible) so I created my own custom responsive css for that component on all devices available on devtools.

--- client/src/components/projects-display/pd-components/sample/pd-card-sample.js
+++ client/src/components/projects-display/pd-components/sample/pd-card-sample.js
@@ -3,26 +3,19 @@
 const sampleCards = [
   {
     id: 1,
+    projName: "KingMaker.io",
+    projWLink: "http://road2fantasychamp.com",
+    projGHLink: "https://github.com/tigerkh3/road2fantasychamp",
+    projDescription: "ESPN Fantasy Basketball Live Analytics Tool",
+  },
+  {
+    id: 2,
     projName: "tigerkh3.io",
     projWLink: "tigerkh3.io",
     projGHLink: "https://github.com/tigerkh3/tigerkh3.io",
     projDescription: "My Personal Portfolio Website built by hand!",
   },
-  {
-  id: 2,
-  projName: "KingMaker.io",
-  projWLink: "https://kingmaker.io",
-  projGHLink: "https://github.com/tigerkh3/road2fantasychamp",
-  projDescription: "ESPN Fantasy Basketball Live Analytics Tool",
-  },
-  {
-    id: 3,
-    projName: "Quiz Whiz",
-    projWLink: "http://35.91.223.95/",
-    projGHLink: "https://github.com/potato-fixers/quiz-whiz",
-    projDescription: "Quiz Whiz an online quiz platform!",
 
-  },
 ]
 
 export default sampleCards;
