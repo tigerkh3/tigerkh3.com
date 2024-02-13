a7e9b8e761a46a4bec08f403c2ba98488977040a
Tiger Hong <tigerhong@Tigers-MBP.lan>
2024-02-12 18:55:49 -0800

    server-dev-1 | Routing
    Basic server file has been set up. New folders created for database operations and routes.
    
    Express server properly serving React Routes.
    
    No need for over complicated routes on the back end since we're resolving it via client sided routing. Express only needs to send over the correct filepath fall back.
    
    Next up is creating our database connection and script files

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
-const express = require("express")
-const app = express();
-const port = 6001;
+// server dependencies
+require("dotenv").config();
+const path = require('path');
+const express = require('express');
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+const app = express();
+app.use(express.json());
+// serve static index.html
+app.use(express.static(path.join(__dirname, "../client/src/dist")))
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/src/dist/index.html"))
 })
 
+
+var port = process.env.PORT || 6001
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
